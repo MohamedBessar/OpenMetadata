598f5a258c00cd98287200b84040ddc3864d1fc8
MohamedBessar <72876612+MohamedBessar@users.noreply.github.com>
2026-01-19 11:01:40 +0200

    Updated .gitignore

--- build/lib/metadata/ingestion/source/database/dremio/service_spec.py
+++
@@ -1,9 +0,0 @@
-
-from metadata.ingestion.source.database.dremio.metadata import DremioSource
-from metadata.utils.service_spec.default import DefaultDatabaseSpec
-
-ServiceSpec = DefaultDatabaseSpec(
-    metadata_source_class=DremioSource,
-    lineage_source_class="not.implemented",
-    usage_source_class="not.implemented",
-)
